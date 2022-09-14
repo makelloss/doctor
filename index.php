f6e1a77badb64bae07233c2b5fa3e45f59673659
makelloss <artemvakhno@gmail.com>
2022-09-14 16:17:51 +0300

    +echo temperature

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $b = 37.7;
 $a = rand(330, 420)/10;
 //$a = 37.7;
-//echo round ($a);
+echo  ($a);
 echo ($a >$b ? "<span style = \" color: red;\">Болен!</span>" : ($a ==$b ? "<span style = \" color: orange;\">Что-то нездоровится…</span>": "<span style = \" color: green;\">Здоров!</span>")); 
 ?>
